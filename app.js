a2511924684254caee9bb409eee1a138b245152a
TRINH DUC DUY <duytd.hanu@gmail.com>
2015-06-16 14:27:36 +0700

    Set redis url and request origin

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-var redis = require("redis").createClient();
+// Initialize redis
+if (process.env.REDISTOGO_URL) {
+    var rtg = require("url").parse(process.env.REDISTOGO_URL);
+    var redis = require("redis").createClient(rtg.port, rtg.hostname);
+    redis.auth(rtg.auth.split(":")[1]);
+} else {
+    var redis = require("redis").createClient();
+}
 
 var cache = {}
 
 redis.subscribe('new-message');
 
 var io = require('socket.io').listen(process.env.PORT || 9000);
+
+io.set('origins', process.env.REQUEST_ORIGIN );
+
 io.on('connection', function(socket){
   console.log('connected socket')
     socket.on('disconnect', function(){
